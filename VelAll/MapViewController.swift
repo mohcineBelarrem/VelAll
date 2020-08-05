aa71c2ce3eb4510db3ad88f142e45d3ab9eb21cf
Mohcine Belarrem <mohcine.belarrem@innovalor.nl>
2020-08-05 23:20:59 +0200

    Adds MapViewController for showing stations in a contract

---
+++ VelAll/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  VelAll
+//
+//  Created by Mohcine BELARREM on 05/08/2020.
+//  Copyright © 2020 Mohcine BELARREM. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    public var contractName : String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = contractName
+        
+    }
+    
+
+    
+
+}
