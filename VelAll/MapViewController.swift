1a14507988362f668f4d3561d489922e90f9db98
Mohcine Belarrem <mohcine.belarrem@innovalor.nl>
2020-08-06 12:16:34 +0200

    Shows the Available bikes in each station for a contract in the map

--- VelAll/MapViewController.swift
+++ VelAll/MapViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import MapKit
+import Contacts
 
 class MapViewController: UIViewController {
     
     public var contractName : String = ""
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = contractName
         
+        mapView.delegate = self
+        
+        mapView.register(MapStationAnnotationView.self, forAnnotationViewWithReuseIdentifier:MKMapViewDefaultAnnotationViewReuseIdentifier)
+        
+        DataRetriever.fetchStations(for: contractName) {result in
+            
+            switch result {
+            
+            case .success(let stationsList):
+                
+                for station  in stationsList {
+                    let mapStation = MapStation(station: station)
+                    self.mapView.addAnnotation(mapStation)
+                }
+                
+                
+                
+                DispatchQueue.main.async {
+                    
+                    guard let firstStation = stationsList.first else { return }
+                    
+                    
+                    let firstStationLocation = CLLocation(latitude: firstStation.position.lat, longitude: firstStation.position.lng)
+                    let region = MKCoordinateRegion(center: firstStationLocation.coordinate,latitudinalMeters: 50000,longitudinalMeters: 60000)
+                    self.mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region),animated: true)
+                    let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+                    self.mapView.setCameraZoomRange(zoomRange, animated: true)
+                    
+                    self.mapView.centerToLocation(firstStationLocation)
+                }
+                
+                
+            case.failure(_):
+                //TODO: Show some feedback
+                print("Error")
+            }
+                
+            }
+            
+        }
+    
+}
+
+
+private extension MKMapView {
+  func centerToLocation(_ location: CLLocation,regionRadius: CLLocationDistance = 3000) {
+    let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                              latitudinalMeters: regionRadius,
+                                              longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
+
+
+
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView,calloutAccessoryControlTapped control: UIControl) {
+        guard let mapStation = view.annotation as? MapStation else {
+            return
+        }
+        
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking]
+        mapStation.mapItem?.openInMaps(launchOptions: launchOptions)
     }
-    
-
-    
-
 }
